44f024bd4bb026d8fb9fcc7a23639a5d6c90bf40
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-12 09:49:57 -0500

    Madlib Insertion

---
+++ madlib.py
@@ -0,0 +1,9 @@
+
+print("Please fill in the blanks below:")
+print("___(name)___'s favorite subject in school is ___(subject)___.")
+
+user_name = input("What is name? ")
+user_subject = input("What is subject? ")
+
+print(f"{user_name}'s favorite subject in school is {user_subject}.")
+
